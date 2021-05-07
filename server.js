392f5c38795c3519b04b77b49bf3fa1003ad1fac
Mohian Dipta <mohianmostofa@gmail.com>
2021-05-07 12:55:30 +0600

    database hookedup

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
+
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true })
 
 
 app.set('view engine', 'ejs')
